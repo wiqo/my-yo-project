d4a8dfb1fde4a87c38ab77589ac532973693df6e
Amol Sidhu <sidhu.amol@gmail.com>
2014-08-05 21:39:44 -0700

    First commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+    $('.pics').slick({
+         slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2400,
+        speed: 800
+    });
+});
